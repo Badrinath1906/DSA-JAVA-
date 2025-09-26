ad0eb8696589813af4c295c9b5b4238273cba97c
Badrinath1906 <badrinathpandey02@gmail.com>
2025-09-27 02:15:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (69.37%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, target, 0, nums.length - 1);
+    }
+
+    private int binarySearch(int[] nums, int target, int left, int right) {
+        if (left > right) return -1;  // base case hai
+
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) return mid;
+
+        // Check if left half is sorted
+        if (nums[left] <= nums[mid]) {
+            if (target >= nums[left] && target < nums[mid]) {
+                return binarySearch(nums, target, left, mid - 1);
+            } else {
+                return binarySearch(nums, target, mid + 1, right);
+            }
+        }
+        // Otherwise, right half is sorted
+        else {
+            if (target > nums[mid] && target <= nums[right]) {
+                return binarySearch(nums, target, mid + 1, right);
+            } else {
+                return binarySearch(nums, target, left, mid - 1);
+            }
+        }
+    }
+}
