181cd1472b8570eba7e38c0021ab0bdc451cab87
Badri Nath Pandey <badrinathpandey02@gmail.com>
2025-12-23 14:53:36 +0530

    Time: 3 ms (88.28%) | Memory: 48.2 MB (53.19%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,13 +1,30 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+        //brute force approach..
+        /*  int m = matrix.length;
+          int n = matrix[0].length;
         for( int i=0; i<m; i++){
             for( int j=0;j<n;j++){
                 if(matrix[i][j]==target) return true;
             }
         }
+        return false;*/
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        // Staircase approach...
+        int row = 0;
+        int col = n - 1; // start at top-right
+
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target)
+                return true;
+            else if (matrix[row][col] > target)
+                col--;      // move left...
+            else
+                row++;      // move down...
+        }
         return false;
-        
+
     }
 }
