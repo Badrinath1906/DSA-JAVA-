7da07869521f704474eee3ec2ce94366d55c730b
Badri Nath Pandey <badrinathpandey02@gmail.com>
2026-05-31 15:08:52 +0530

    Time: 1 ms (82.29%) | Memory: 42.6 MB (72.00%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length) {
+            return false;
+        }
+        HashMap<Character, String> map1 = new HashMap<>();
+        HashMap<String, Character> map2 = new HashMap<>();
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            if (map1.containsKey(ch) && !map1.get(ch).equals(words[i]))
+                return false;
+            if (map2.containsKey(words[i]) && map2.get(words[i]) != ch)
+                return false;
+            map1.put(ch, words[i]);
+            map2.put(words[i], ch);
+        }
+
+        return true;
+
+    }
+}
