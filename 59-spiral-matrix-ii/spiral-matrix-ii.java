b86be3da2ec99205269300eb31b0401b12b4b7f1
Badri Nath Pandey <badrinathpandey02@gmail.com>
2026-07-29 23:26:19 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (24.19%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -1,44 +1,38 @@
 class Solution {
-    public int[][] generateMatrix(int n){
-        int [][] matrix= new int[n][n];
-        int top= 0;
-        int bottom= n-1;
-        int left=0;
-        int right=n-1;
-         int current =1;
-        while(top<=bottom && left<=right){
-
-            for( int i=left;i<=right;i++){
-                matrix[top][i]= current;
-                current++;
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int top = 0;
+        int bottom = n - 1;
+        int left = 0;
+        int right = n - 1;
+        int num = 1;
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = num;
+                num++;
             }
-             top++;
-
-            for(int i=top;i<=bottom;i++ ) {
-                matrix[i][right]=current;
-                current++;
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num;
+                num++;
             }
-             right--;
-            
-            if(top<=bottom){
-            for(int i= right;i>=left;i--){
-                matrix[bottom][i]= current;
-                current++;
-            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    matrix[bottom][i] = num;
+                    num++;
+                }
                 bottom--;
             }
-       
-             if(left<=right){
-                for( int i=bottom;i>=top;i--){
-                    matrix[i][left]= current;
-                    current++;
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = num;
+                    num++;
                 }
                 left++;
-             }
+            }
         }
+        return matrix;
 
-         return matrix;
-        }
+    }
 }
-        
-        
