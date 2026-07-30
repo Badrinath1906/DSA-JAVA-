c3068ff28589bacc1eb9c41cc0d67e57e81a39d2
Badri Nath Pandey <badrinathpandey02@gmail.com>
2026-07-30 19:23:23 +0530

    Time: 130 ms (73.35%) | Memory: 43 MB (60.59%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,36 +1,51 @@
 class Solution {
-    public boolean exist(char[][] board, String word) {
-        int m= board.length;
-        int n= board[0].length;
-        char[] arr =word.toCharArray();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]==arr[0]&& search( board,arr, i, j,0))  return true;
 
+    public boolean exist(char[][] board, String word) {
+
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+
+                if (board[r][c] == word.charAt(0)) {
+
+                    if (dfs(board, r, c, word, 0))
+                        return true;
+                }
             }
         }
+
         return false;
     }
 
-    public boolean search(char[][] board, char[] arr, int i, int j, int k) {
-        //Base Case..
-        if(k==arr.length) return true;
+    public boolean dfs(char[][] board, int r, int c, String word, int wordIndex) {
 
-        //Boundary check..
-        if (i < 0 || i >= board.length || j >= board[0].length || j < 0 || board[i][j] != arr[k])
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // Out of bounds
+        if (r < 0 || r >= rows || c < 0 || c >= cols)
             return false;
-            
-        //mark visited..
-        char temp = board[i][j];
-        board[i][j] = '*';
 
-        boolean found = search(board, arr, i + 1, j, k+1) ||
-                search(board, arr, i - 1, j, k+1) ||
-                search(board, arr, i, j + 1, k+1) ||
-                search(board, arr, i, j - 1, k+1);
+        // Invalid case (visited or character mismatch)
+        if (board[r][c] == ' ' || board[r][c] != word.charAt(wordIndex))
+            return false;
 
-        //backtrack kr lo dubara se us postion ko ..
-        board[i][j] = temp;
+        // Last character matched..
+        if (wordIndex == word.length() - 1)
+            return true;
+
+        // Mark as visited...
+        char ch = board[r][c];
+        board[r][c] = ' ';
+
+        // DFS in 4 directions..
+        boolean found =
+                dfs(board, r - 1, c, word, wordIndex + 1) ||
+                dfs(board, r + 1, c, word, wordIndex + 1) ||
+                dfs(board, r, c - 1, word, wordIndex + 1) ||
+                dfs(board, r, c + 1, word, wordIndex + 1);
+
+        // Backtrack...
+        board[r][c] = ch;
         return found;
     }
 }
