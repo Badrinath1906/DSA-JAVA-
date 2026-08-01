770651de5af919df4b36f1f71b118430ff19a0d0
Badri Nath Pandey <badrinathpandey02@gmail.com>
2026-08-01 17:10:53 +0530

    Time: 4 ms (36.92%) | Memory: 43.2 MB (58.24%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,36 +1,21 @@
-import java.util.Stack;
-
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-        int n = s.length();
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-
-            // push opening brackets
             if (ch == '(' || ch == '{' || ch == '[') {
                 st.push(ch);
-            } 
-            // handle closing brackets
-            else {
-                if (st.size() == 0) return false;
-
-                char top = st.peek();
-
-                if ((ch == ')' && top == '(') ||
-                    (ch == '}' && top == '{') ||
-                    (ch == ']' && top == '[')) {
-                    st.pop();
-                } 
-                else {
+            } else {
+                if (st.size() == 0)
                     return false;
-                }
+                char top = st.pop();
+                if (ch == ')' && top != '(' ||
+                        ch == '}' && top != '{' ||
+                        ch == ']' && top != '[')
+                    return false;
             }
         }
+        return st.isEmpty();
 
-        // if any unmatched opening brackets remain  false
-        if (st.size() > 0) return false;
-        else return true;
     }
 }
