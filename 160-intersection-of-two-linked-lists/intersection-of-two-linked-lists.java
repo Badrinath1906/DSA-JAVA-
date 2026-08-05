40ea5b0380b401b74b2cd70a6731f3c0770bfa45
Badri Nath Pandey <badrinathpandey02@gmail.com>
2026-08-05 11:04:00 +0530

    Time: 1 ms (99.93%) | Memory: 52.9 MB (27.64%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,38 +11,36 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode tempA= headA;
-        ListNode tempB= headB;
-        int size1=0;
-        while(tempA!=null){
-            size1++;
-            tempA=  tempA.next;
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        int sizeA = 0;
+        while (tempA != null) {
+            sizeA++;
+            tempA = tempA.next;
         }
-         int size2= 0;
-        while(tempB!=null){
-            size2++;
-            tempB=  tempB.next;
+        int sizeB = 0;
+        while (tempB != null) {
+            sizeB++;
+            tempB = tempB.next;
         }
-        tempA =  headA;
-        tempB =  headB;
-        if(size1>size2){
-            int steps=size1-size2;
-            for( int i=1; i<=steps;i++){
-                tempA= tempA.next;
-              } 
-        }
-         else{
-            int steps=size2-size1;
-            for( int i=1; i<=steps;i++){
-                tempB= tempB.next;
+        tempA = headA;
+        tempB = headB;
+        if (sizeA > sizeB) {
+            int steps = sizeA - sizeB;
+            for (int i = 0; i < steps; i++) {
+                tempA = tempA.next;
+            }
+        } else {
+            int steps = sizeB - sizeA;
+            for (int i = 0; i < steps; i++) {
+                tempB = tempB.next;
             }
         }
-        while(tempA!=tempB){
-            tempA=tempA.next;
-            tempB=tempB.next;
+        while (tempA != tempB) {
+            tempA = tempA.next;
+            tempB = tempB.next;
         }
-        return tempA;
-        
-        
+        return tempB;
+
     }
 }
