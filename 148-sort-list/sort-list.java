27ea470ba9b5eb174c9fa3e8e19eeef8da17ae24
Badri Nath Pandey <badrinathpandey02@gmail.com>
2026-08-08 16:53:12 +0530

    Time: 15 ms (15.09%) | Memory: 63 MB (14.09%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,22 +10,22 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if (head==null) return null;
-        List<Integer> list= new ArrayList<>();
+        ArrayList<Integer>list = new ArrayList<>();
         ListNode temp = head;
-
+        if(head==null) return null;
         while(temp!=null){
             list.add(temp.val);
             temp= temp.next;
         }
         Collections.sort(list);
-        //Rebuild linked list
-        temp= head;
-        for( int val:list){
-            temp.val= val;
-            temp= temp.next;
+        //Rebuild..
+        temp = head;
+        for(int num:list){
+            temp.val = num;
+            temp = temp.next;
         }
-         return head;
+        return head;
+
         
     }
 }
