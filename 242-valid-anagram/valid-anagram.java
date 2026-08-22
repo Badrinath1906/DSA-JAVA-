5c83cb083c08f226a24c1907a6453720f5a5680c
Badri Nath Pandey <badrinathpandey02@gmail.com>
2026-08-22 16:04:04 +0530

    Time: 15 ms (23.95%) | Memory: 46.3 MB (47.74%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
+        if(s.length()!= t.length())  return false;
         HashMap<Character,Integer>map = new HashMap<>();
         for(char ch:s.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            map.put(ch, map.getOrDefault(ch, 0)+1);
         }
-        for(char c:t.toCharArray()){
-            if(!map.containsKey(c)||map.get(c)==0)  return false;
-            map.put(c, map.get(c)-1);
+        for(char ch: t.toCharArray()){
+            if(!map.containsKey(ch) || map.get(ch)==0) return false;
+            map.put(ch,map.get(ch)-1);
         }
         return true;
-        
     }
 }
